297b22214e159758ed37c2f5a2271021bce92c9e
John S. Erickson, Ph.D <olyerickson@gmail.com>
2018-05-23 11:09:03 -0400

    Added additional  monitor
    Added support for additional monitor and centered window content

--- campfire_demo.R
+++ campfire_demo.R
@@ -1,3 +1,6 @@
+# set working directory - only works in RStudio (with rstudioapi)
+#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 source("campfire_lib.R")
 
 campfireApp(
@@ -6,22 +9,48 @@ campfireApp(
     h1("Controller"),
     selectInput(inputId = "fruitSelection",
       label = "Pick a fruit",
-      choices = c("Apple", "Banana", "Pear"))
+      choices = c("Apple", "Banana", "Pear")),
+    style="position: absolute; 
+           top: 50%; left: 50%; 
+           margin-right: -50%; 
+           transform: translate(-50%, -50%)"
   ),
   
   wall = div(
     h1("Wall"),
-    textOutput("wallText")
+    textOutput("wallText"),
+    style="position: absolute; 
+           top: 50%; left: 50%;
+           margin-right: -50%; 
+           transform: translate(-50%, -50%);
+           background: rgb(255, 255, 255);"
   ),
   
   floor = div(
     h1("Floor"),
-    textOutput("floorText")
+    textOutput("floorText"),
+    style="position: absolute; 
+           top: 50%; left: 50%;
+           margin-right: -50%; 
+           transform: translate(-50%, -50%);
+           background: rgb(255, 255, 255);"
+  ),
+
+  monitor = div(
+    h1("External Monitor"),
+    textOutput("monitorText"),
+    style="position: absolute; 
+           top: 50%; left: 50%;
+           margin-right: -50%; 
+           transform: translate(-50%, -50%);
+           background: rgb(255, 255, 255);"
   ),
   
   serverFunct = function(serverValues, output) {
     output$wallText <- renderText({ serverValues$fruitSelection })
     output$floorText <- renderText({ serverValues$fruitSelection })
+    output$monitorText <- renderText({ serverValues$fruitSelection })
   }
   
 )
+
